c6bf78933693b877e735589d7a70f56f780f592c
Taimoor Anwar <77386699+taimoor889@users.noreply.github.com>
2025-06-18 19:09:39 +0200

    entry point for the application

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,15 @@
-import express from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
+import authRoutes from './routes/auth.routes.js'
+import connectDB from "./config/db.js";
+import errorHandler from "./middlewares/errorMiddleware.js";
 dotenv.config(); 
+connectDB();
 const app =express();
+app.use(express.json());
 const port= process.env.PORT||3000;
+app.use(errorHandler);
+app.use('/api/auth/',authRoutes);
 
 app.listen(port, ()=>{
     console.log(`server is runing on the port ${port}`);
